eb7a651ddade5af370cc696cc628f322d04e6512
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-10-14 11:32:01 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
  * _printf - is a fuction that selects the correct function to print.
@@ -11,4 +12,17 @@ int _printf(const char *format, ...)
 	convert_match m[] = {
 		{"%s", printf_string}, {"%c", printf_char},
 		{"%%", printf_37},
-		{
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_Hex},
+		{"%s", printF_exclusive_string}, {"%p", printf_pointer}
+	};
+
+	va_list list;
+	int i = 0, j, len = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+Here:
+}
